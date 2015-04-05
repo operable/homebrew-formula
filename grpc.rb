ed0dde1a2df791c2c65e07d739325745b282d1dc
Will Farrington <wfarrington@digitalocean.com>
2015-04-05 00:18:38 -0400

    grpc depends on protof:devel to build

--- grpc.rb
+++ grpc.rb
@@ -2,6 +2,8 @@ class Grpc < Formula
   url 'git://github.com/grpc/grpc.git', :revision => '62d637c16383b63f084dbdd230791fac9727175e'
   version '62d637c16383b63f084dbdd230791fac9727175e'
 
+  depends_on "protobuf" => "devel"
+
   def install
     system "git submodule update --init"
     system "make prefix=#{prefix}"
