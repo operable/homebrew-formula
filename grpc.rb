6c9cb6acf9f9e180aae20f3dfd8b2bc7981123f9
Will Farrington <wfarrington@digitalocean.com>
2015-04-05 00:16:33 -0400

    initial commit

---
+++ grpc.rb
@@ -0,0 +1,10 @@
+class Grpc < Formula
+  url 'git://github.com/grpc/grpc.git', :revision => '62d637c16383b63f084dbdd230791fac9727175e'
+  version '62d637c16383b63f084dbdd230791fac9727175e'
+
+  def install
+    system "git submodule update --init"
+    system "make prefix=#{prefix}"
+    system "make install prefix=#{prefix}"
+  end
+end
