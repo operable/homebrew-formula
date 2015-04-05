4974b254c0fefa4c3dcf0342fc977b1f62f939a4
Will Farrington <wfarrington@digitalocean.com>
2015-04-05 00:39:52 -0400

    fighting homebrew more

--- grpc.rb
+++ grpc.rb
@@ -2,11 +2,12 @@ class Grpc < Formula
   url 'git://github.com/grpc/grpc.git', :revision => '300ebc4c623b46f45506ac14b31e2660196dc596'
   version '300ebc4c623b46f45506ac14b31e2660196dc596'
 
+  depends_on "autoconf"
   depends_on "protobuf" # devel
 
   def install
     system "git submodule update --init"
-    system "make shared_c prefix=#{prefix}"
+    system "make"
     system "make install prefix=#{prefix}"
   end
 end
